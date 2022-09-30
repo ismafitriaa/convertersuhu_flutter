2ebdab5eef0568a0080146cf0c6c7d3287b46c01
ismafitriaa <risnandari1212@gmail.com>
2022-09-30 22:12:53 +0700

    converter-suhu-listview

--- lib/main.dart
+++ lib/main.dart
@@ -4,112 +4,271 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
+  State<MyApp> createState() => _MyAppState();
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
+class _MyAppState extends State<MyApp> {
+  var items = ['Kelvin', 'Reamor', 'Farenheit', 'Semua'];
+  String dropdownvalue = 'Kelvin';
+  String? selectedValue;
+  double celcius = 0;
+  double kelvin = 0;
+  double reamor = 0;
+  double farenheit = 0;
+  double output = 0;
+  double output1 = 0;
+  double output2 = 0;
+  double output3 = 0;
+  List<String> listViewItem1 = <String>[];
+  List<String> listViewItem2 = <String>[];
+  List<String> listViewItem3 = <String>[];
+  List<String> listViewItem4 = <String>[];
+  TextEditingController inputSuhu = TextEditingController();
+  konversi() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      celcius = double.parse(inputSuhu.text);
+      if (dropdownvalue == 'Kelvin') {
+        output = 273.15 + celcius;
+        reamor = 0;
+        farenheit = 0;
+        kelvin = output;
+      } else if (dropdownvalue == 'Reamor') {
+        output = 4 / 5 * celcius;
+        reamor = output;
+        farenheit = 0;
+        kelvin = 0;
+      } else if (dropdownvalue == 'Farenheit') {
+        output = (9 / 5 * celcius) + 32;
+        reamor = 0;
+        farenheit = output;
+        kelvin = 0;
+      } else {
+        output1 = 273.15 + celcius;
+        output2 = 4 / 5 * celcius;
+        output3 = (9 / 5 * celcius) + 32;
+        reamor = output2;
+        farenheit = output3;
+        kelvin = output1;
+      }
+      listViewItem1.add('$reamor');
+      listViewItem2.add('$kelvin');
+      listViewItem3.add('$farenheit');
+      listViewItem4.add('$celcius');
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
+      home: Scaffold(
+        resizeToAvoidBottomInset: false,
+        appBar: AppBar(
+          title: const Text('Kalkulator'),
+        ),
+        body: Column(
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+            Expanded(
+              flex: 2,
+              child: Container(
+                margin: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Container(child: inputan()),
+                    Container(
+                      child: dropDown(),
+                    ),
+                    Container(
+                      child: outputSuhu(),
+                    ),
+                    Container(
+                      child: buttonSubmit(),
+                    ),
+                  ],
+                ),
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            const Expanded(
+              flex: 0,
+              child: Text(
+                'Riwayat Konversi',
+                style: TextStyle(fontSize: 20),
+              ),
+            ),
+            const SizedBox(
+              height: 15,
+            ),
+            Expanded(
+              flex: 2,
+              child: listHasil(),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+
+  inputan() {
+    return TextFormField(
+      controller: inputSuhu,
+      keyboardType: TextInputType.number,
+      decoration: const InputDecoration(
+        border: UnderlineInputBorder(),
+        labelText: 'Masukan Suhu Dalam Celcius',
+      ),
+    );
+  }
+
+  dropDown() {
+    return DropdownButton(
+      value: dropdownvalue,
+      icon: const Icon(Icons.keyboard_arrow_down),
+      items: items.map((String items) {
+        return DropdownMenuItem(
+          value: items,
+          child: Text(items),
+        );
+      }).toList(),
+      onChanged: (String? newValue) {
+        setState(() {
+          dropdownvalue = newValue!;
+        });
+      },
+    );
+  }
+
+  outputSuhu() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        Expanded(
+          flex: 1,
+          child: Column(
+            children: [
+              const Text('Suhu dalam Reamur',
+                  style: TextStyle(
+                    fontSize: 15,
+                  ),
+                  textAlign: TextAlign.center),
+              const SizedBox(height: 20),
+              Text(
+                "$reamor",
+                style: const TextStyle(fontSize: 25),
+              )
+            ],
+          ),
+        ),
+        Expanded(
+          flex: 1,
+          child: Column(
+            children: [
+              const Text(
+                'Suhu dalam \nKelvin',
+                style: TextStyle(fontSize: 15),
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(height: 20),
+              Text(
+                "$kelvin",
+                style: const TextStyle(fontSize: 25),
+              )
+            ],
+          ),
+        ),
+        Expanded(
+          flex: 1,
+          child: Column(
+            children: [
+              const Text('Suhu dalam \nFahrenheit',
+                  style: TextStyle(fontSize: 15), textAlign: TextAlign.center),
+              const SizedBox(height: 20),
+              Text(
+                "$farenheit",
+                style: const TextStyle(fontSize: 25),
+              )
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+
+  buttonSubmit() {
+    return SizedBox(
+        width: double.infinity,
+        child: ElevatedButton(
+          child: const Text('Submit'),
+          onPressed: () => konversi(),
+        ));
+  }
+
+  listHasil() {
+    return SizedBox(
+      height: 120,
+      child: ListView.builder(
+        shrinkWrap: true,
+        itemCount: listViewItem4.length,
+        itemBuilder: (context, index) => Container(
+          decoration:
+              BoxDecoration(border: Border.all(color: Colors.lightBlueAccent)),
+          margin: const EdgeInsets.fromLTRB(0, 0, 0, 10),
+          child: Row(children: <Widget>[
+            Expanded(
+              flex: 4,
+              child: Column(
+                children: [Text((index + 1).toString())],
+              ),
+            ),
+            Expanded(
+              flex: 10,
+              child: Row(
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: const [
+                      Text(
+                        'Celcius',
+                        style:
+                            TextStyle(fontWeight: FontWeight.bold, height: 2),
+                      ),
+                      Text(
+                        'Reamur\t',
+                      ),
+                      Text('Kelvin\t'),
+                      Text('Fahrenheit\t'),
+                      SizedBox(
+                        height: 10,
+                      )
+                    ],
+                  ),
+                  Column(children: const [
+                    Text('=\t'),
+                    Text('=\t'),
+                    Text('=\t'),
+                    Text('=\t'),
+                  ]),
+                  Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(listViewItem4.elementAt(index)),
+                        Text(listViewItem1.elementAt(index)),
+                        Text(listViewItem2.elementAt(index)),
+                        Text(listViewItem3.elementAt(index)),
+                      ]),
+                ],
+              ),
+            ),
+          ]),
+        ),
+      ),
     );
   }
 }
